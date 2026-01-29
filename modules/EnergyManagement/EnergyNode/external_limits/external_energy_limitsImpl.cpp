7b23a35b5bc28cd66db889c1f2c8d49a8cae01de
pow-elk-arge <powerelektronik.app.acc@gmail.com>
2026-01-29 18:38:06 +0300

    external_limits log added #v12

--- modules/EnergyManagement/EnergyNode/external_limits/external_energy_limitsImpl.cpp
+++ modules/EnergyManagement/EnergyNode/external_limits/external_energy_limitsImpl.cpp
@@ -2,6 +2,7 @@
 // Copyright Pionix GmbH and Contributors to EVerest
 
 #include "external_energy_limitsImpl.hpp"
+#include <everest/logging.hpp>
 
 namespace module {
 namespace external_limits {
@@ -13,7 +14,29 @@ void external_energy_limitsImpl::ready() {
 }
 
 void external_energy_limitsImpl::handle_set_external_limits(types::energy::ExternalLimits& value) {
-    // your code for cmd set_external_limits goes here
+    auto log_limits = [](const char* label, const types::energy::ScheduleReqEntry& entry) {
+        const auto& l = entry.limits_to_leaves;
+        if (l.total_power_W.has_value()) {
+            EVLOG_info << "[ExternalLimits] " << label << " power_W=" << l.total_power_W->value
+                       << " source=" << l.total_power_W->source;
+        }
+        if (l.ac_max_current_A.has_value()) {
+            EVLOG_info << "[ExternalLimits] " << label << " current_A=" << l.ac_max_current_A->value
+                       << " source=" << l.ac_max_current_A->source;
+        }
+        if (l.ac_max_phase_count.has_value()) {
+            EVLOG_info << "[ExternalLimits] " << label << " phases=" << l.ac_max_phase_count->value
+                       << " source=" << l.ac_max_phase_count->source;
+        }
+    };
+
+    if (!value.schedule_import.empty()) {
+        log_limits("import", value.schedule_import.front());
+    }
+    if (!value.schedule_export.empty()) {
+        log_limits("export", value.schedule_export.front());
+    }
+
     mod->signalExternalLimit(value);
 };
 
