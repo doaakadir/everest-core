e7d6c89a864a70dcb2df5d89ea5fc0e2dde54c76
pow-elk-arge <powerelektronik.app.acc@gmail.com>
2026-07-30 10:53:31 +0300

    fix in slac qca pollling debug mechanism #v52

--- modules/Testing/DummyTokenProviderManual/main/auth_token_providerImpl.cpp
+++ modules/Testing/DummyTokenProviderManual/main/auth_token_providerImpl.cpp
@@ -9,15 +9,18 @@ namespace module {
 namespace main {
 
 void auth_token_providerImpl::init() {
-    mod->mqtt.subscribe("everest_api/dummy_token_provider/cmd/provide", [this](const std::string& msg) {
+    auto handle_provided_token = [this](const std::string& msg) {
         try {
             types::authorization::ProvidedIdToken token = json::parse(msg);
-            EVLOG_info << "Publishing new dummy token: " << everest::helpers::redact(token);
+            EVLOG_info << "Publishing externally provided token: " << everest::helpers::redact(token);
             publish_provided_token(token);
         } catch (const nlohmann::json::exception& e) {
             EVLOG_error << "Failed to handle JSON token from MQTT: " << e.what();
         }
-    });
+    };
+
+    mod->mqtt.subscribe("everest_api/local_authorization/cmd/provide", handle_provided_token);
+    mod->mqtt.subscribe("everest_api/dummy_token_provider/cmd/provide", handle_provided_token);
 }
 
 void auth_token_providerImpl::ready() {
